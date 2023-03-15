c724febd949bf2bf6f0d099427dc9f4284421945
Fehlpe <felpearcanjo@gmail.com>
2023-03-15 18:16:08 -0300

    fix: add user route

--- src/main/server/express.routes.ts
+++ src/main/server/express.routes.ts
@@ -1,3 +1,6 @@
 import { Express } from "express";
+import { userRoutes } from "../../app/features/user/routes/user.routes";
 
-export const makeRoutes = (app: Express) => {};
+export const makeRoutes = (app: Express) => {
+  app.use("/user", userRoutes);
+};
