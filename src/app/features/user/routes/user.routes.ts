0afbb1cc517892ed59ef72969892f4cd71546aff
Fehlpe <felpearcanjo@gmail.com>
2023-03-15 21:42:24 -0300

    fix: passwords validator

--- src/app/features/user/routes/user.routes.ts
+++ src/app/features/user/routes/user.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 import { checkDuplicateUserValidator } from "../validators/checkDuplicateUser.validator";
+import { checkPasswordsValidator } from "../validators/checkPasswords.validator";
 import { createUserValidator } from "../validators/createUser.validator";
 
 const userRoutes = Router();
 
 userRoutes.post(
   "/",
-  [createUserValidator, checkDuplicateUserValidator],
+  [createUserValidator, checkDuplicateUserValidator, checkPasswordsValidator],
   new UserController().create
 );
 
